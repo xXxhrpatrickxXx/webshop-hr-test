ecace128042676288379886478046526b6496a5d
Mikkel <Mikkelmbk@live.dk>
2022-02-11 21:47:16 +0100

    implemented swiper slider and prepared static html structure as a template for when the backend is set up and data will be pulled from there.

---
+++ Assets/JS/swiper-frames.js
@@ -0,0 +1,49 @@
+const swiper = new Swiper(".js-frames-slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    autoplay:{
+        delay:3000,
+    },
+    speed:750,
+    autoHeight:true,
+    pauseOnMouseEnter: true,
+    clickable: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      457: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      667: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      962: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1237: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1511: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      1770: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination.c-swiper__pagination",
+      type:"bullets",
+    },
+  });
+  
