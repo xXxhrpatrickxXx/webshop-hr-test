f713d5288a2cb8e6f2e7c235d3e63c73bfb7c1f5
Mads <kontaktmadsen420@gmail.com>
2022-03-12 22:11:22 +0100

    Vi har rettet desktop søgebar og oprettet en søge JavaScript fil og en søge HTML fil og klargjort action attributter i alle HTML filer.

---
+++ Assets/JS/search.js
@@ -0,0 +1,10 @@
+let formSearchElem = document.querySelector(".c-header__form");
+
+formSearchElem.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    console.log(window.location);
+    // let query = 
+
+    // formSearchElem.submit();
+});
